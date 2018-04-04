f9b0ff4a0ac92e4ed19b7b0ead5cf49a1b829bf1
colinbankier <colinbankier@gmail.com>
2018-04-04 23:01:41 +1000

    Add docs for static handlers and do rustfmt.

--- gotham/src/handler/mod.rs
+++ gotham/src/handler/mod.rs
@@ -11,6 +11,8 @@ use hyper::{Body, Response};
 use state::State;
 
 mod error;
+
+/// Defines handlers for serving static files.
 pub mod static_file;
 
 pub use self::error::{HandlerError, IntoHandlerError};
